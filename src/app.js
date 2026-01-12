de84c238b6aa07aa2d1ab96dffd9995486bceeef
Chettha.pj <chettha.pj@gmail.com>
2026-01-12 13:26:53 +0700

    added https://fount-end.vercel.app/

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
+    "https://fount-end.vercel.app",
     "https://jsd-react-assessment-solution-gilt.vercel.app",
   ],
   credentials: true, // ✅ allow cookies to be sent
